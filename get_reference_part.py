9abd07351c124746a65fa0277673c56dd8b434ad
dmm2017 <meleshko.dmitrii@gmail.com>
2018-07-31 14:24:48 -0400

    Make passing parameters easier

--- get_reference_part.py
+++ get_reference_part.py
@@ -2,13 +2,26 @@ import sys
 from Bio import SeqIO, SeqRecord
 #usage:
 #python get_reference_part.py <reference.fasta> <chromosome name> <start_position> <end_position> <out_dir>
-chr_name = sys.argv[2]
+#or
+#python get_reference_part.py <reference.fasta> <chrname_start_end> <out_dir>
+if len(sys.argv) == 6:
+    chr_name = sys.argv[2]
+    start = int(sys.argv[3])
+    end = int(sys.argv[4])
+    out_dir = sys.argv[5]
+elif len(sys.argv) == 4:
+    splitted = sys.argv[2].split("_")
+    chr_name = splitted[0]
+    start = int(splitted[1])
+    end = int(splitted[2])
+    out_dir = sys.argv[3]
+
 subseq = None
 for record in SeqIO.parse(sys.argv[1], "fasta"):
     if record.id == chr_name:
-        subseq = record.seq[int(sys.argv[3]):int(sys.argv[4])]
+        subseq = record.seq[start:end]
 
-out_filename = sys.argv[5] + "/" + chr_name + "_" + sys.argv[3] + "_" + sys.argv[4] + ".fasta"
+out_filename = out_dir + "/" + chr_name + "_" + str(start) + "_" + str(end) + ".fasta"
 SeqIO.write([SeqRecord("subseq", subseq)], out_filename, "fasta")
 
 
