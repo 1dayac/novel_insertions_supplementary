f3966de813e905832a0da6d8073765be5bb08142
dmm2017 <meleshko.dmitrii@gmail.com>
2020-03-05 17:15:27 -0500

    new insertion set

--- simulation/simulate_insertions.py
+++ simulation/simulate_insertions.py
@@ -1,11 +1,10 @@
 #Usage
 #simulate_insertions.py num_of_insertions min_len max_len out_file
 
-
-
-
+import numpy as np
 import sys
-
+from random import choice
+from random import random, randrange
 
 chrs = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10",
         "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20",
@@ -15,11 +14,26 @@ lengths = [248956422, 242193529, 198295559, 190214555, 181538259, 170805979, 159
            135086622, 133275309, 114364328, 107043718, 101991189, 90338345, 83257441, 80373285, 58617616, 64444167,
            46709983, 50818468, 156040895]
 
-num_of_insertions = sys.argv[1]
-min_length = sys.argv[2]
-max_length = sys.argv[3]
+cum_lengths = np.cumsum(lengths)
+
+def get_chrom_pos(pos):
+    for i in range(len(chrs)):
+        if cum_lengths[i] > pos:
+            if i != 0:
+                return chrs[i], pos - cum_lengths[i-1]
+            else:
+                return chrs[i], pos
+
+
+num_of_insertions = int(sys.argv[1])
+min_length = int(sys.argv[2])
+max_length = int(sys.argv[3])
 out_file = sys.argv[4]
 
-with open(out_file, 'w'):
+
+with open(out_file, 'w') as out:
     for i in range(num_of_insertions):
-        pass
+        chrom, pos = get_chrom_pos(randrange(sum(lengths)))
+        ins_len = randrange(min_length, max_length)
+        ins = ''.join(choice(["A", "C", "G", "T"]) for i in range(ins_len))
+        out.write(str(chrom) + "/" + str(pos) + "\t" + ins + "\n")
